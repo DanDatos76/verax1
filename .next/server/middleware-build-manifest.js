8e44d07e3226663649e5823a41e66965c3c3b824
Daniel Paez <labotica@laboticahits.com.ar>
2025-11-20 10:38:23 -0300

    instalacion librerias

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -4,21 +4,9 @@ globalThis.__BUILD_MANIFEST = {
   },
   "devFiles": [],
   "ampDevFiles": [],
-  "polyfillFiles": [
-    "static/chunks/b0096_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_408a2387._.js",
-    "static/chunks/b0096_next_dist_compiled_react-dom_a8398abe._.js",
-    "static/chunks/b0096_next_dist_compiled_next-devtools_index_7eb4c47b.js",
-    "static/chunks/b0096_next_dist_compiled_d51edd42._.js",
-    "static/chunks/b0096_next_dist_client_9840c103._.js",
-    "static/chunks/b0096_next_dist_1d9bcb5f._.js",
-    "static/chunks/b0096_@swc_helpers_cjs_5346d1b3._.js",
-    "static/chunks/seo_verax1_a0ff3932._.js",
-    "static/chunks/turbopack-seo_verax1_85d8ad77._.js"
-  ],
+  "rootMainFiles": [],
   "ampFirstPages": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
